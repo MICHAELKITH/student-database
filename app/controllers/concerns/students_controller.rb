a832f1707ccce121952f0d67384518aa713cd97b
MICHAELKITH <michaelkithinji27@gmail.com>
2023-09-23 14:32:28 +0300

    Created Form to save new students

--- app/controllers/concerns/students_controller.rb
+++ app/controllers/concerns/students_controller.rb
@@ -6,4 +6,23 @@ class StudentsController < ApplicationController
     def new
         @student = Student.new
     end
+
+    def create
+        @student = Student.new(
+            # first_name: params[:student] [:first_name],
+            # last_name: params[:student] [:last_name],
+            # email: params[:student] [:email]
+            student_params
+        )
+
+        if @student.save()
+            redirect_to students_path
+        else
+            render :new
+        end
+    end
+    private 
+    def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+    validates :first_name, :last_name, :email, presence: true
 end
+  
