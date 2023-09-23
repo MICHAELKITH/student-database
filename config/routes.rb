f3359b7041345b9e42f0df294d35c481eb50bac4
MICHAELKITH <michaelkithinji27@gmail.com>
2023-09-23 13:48:13 +0300

    Created Student model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   # get "welcome/index"
   root "welcome#index"
+  resources :students
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+student =Student.create(
+    first_name: 'Mike',
+    last_name: 'kithinji',
+    email: 'mikecode945@gmail.com'
+)
+20.times do |i|
+    puts "Creating Students #{i+1}"
+    Student.create(
+        first_name: "Student #{i+1}",
+        last_name: "Lname#{i+1}",
+        email: "Student #{i+1}@gmail.com"
+    )
+end
