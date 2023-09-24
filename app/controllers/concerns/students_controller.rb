9a79993824efe80eef8b8c9542a2c38d80d5745c
MICHAELKITH <michaelkithinji27@gmail.com>
2023-09-24 11:24:59 +0300

    Edit students

--- app/controllers/concerns/students_controller.rb
+++ app/controllers/concerns/students_controller.rb
@@ -1,37 +1,46 @@
 class StudentsController < ApplicationController
-    def index
-        @students = Student.all
-    end
+  def index
+    @students = Student.all
+  end
 
-    def new
-        @student = Student.new
-    end
+  def new
+    @student = Student.new
+  end
 
-    def create
-        @student = Student.new(
-            # first_name: params[:student] [:first_name],
-            # last_name: params[:student] [:last_name],
-            # email: params[:student] [:email]
-            student_params
-        )
+  # Creating new student
+  def create
+    @student = Student.new(
+      # first_name: params[:student] [:first_name],
+      # last_name: params[:student] [:last_name],
+      # email: params[:student] [:email]
+      student_params
+    )
 
-        if @student.save()
-            redirect_to students_path
-        else
-            render :new
-        end
+    if @student.save()
+      redirect_to students_path
+    else
+      render :new
     end
-    def show
-        @student = Student.find_by(id: params[:id])
-      
-        if @student.nil?
-          flash[:alert] = "Student not found."
-          redirect_to students_path # Redirect to the index page or another appropriate page
-        end
-      end
-      
-    private 
-    def student_params
+  end
+
+  # View new student
+  def show
+    @student = Student.find_by(id: params[:id])
+
+    if @student.nil?
+      flash[:alert] = "Student not found."
+      redirect_to students_path # Redirect to the index page or another appropriate page
+    end
+  end
+
+  def edit 
+    @student = Student.find_by(id: params[:id])
+  end
+
+  #   Requirement params
+  private
+
+  def student_params
     params.require(:student).permit(:first_name, :last_name, :email)
-    end
+  end
 end
